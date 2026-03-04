3b108c305011675c44b0d6dc0ed6dd00ce44f875
afwarifa <afwatikarifa@gmail.com>
2026-03-04 12:21:12 +0700

    feat: add view sidebar

---
+++ application/views/layout/sidebar.php
@@ -0,0 +1,32 @@
+<!-- SIDEBAR -->
+<aside class="w-64 bg-white shadow-lg">
+    <div class="flex justify-center mb-2 p-6">
+        <img src="<?= base_url('assets/img/logo.png') ?>"
+            alt="SmartRAB Logo"
+            class="w-36 h-auto object-contain">
+    </div>
+
+    <div class="flex flex-col items-center mb-6 p-6">
+        <div class="w-20 h-20 bg-gray-300 rounded-full mb-3"></div>
+        <p class="font-semibold">Prime Leonard</p>
+        <span class="text-sm text-gray-400">user</span>
+    </div>
+
+    <nav class="space-y-3">
+        <div class="flex gap-2 pr-6" style="height:50px;">
+            <div class="block px-1 py-2 rounded-lg bg-blue-500 text-white">
+            </div>
+            <a href="#" class="flex items-center justify-center w-full px-4 pr-6 rounded-lg bg-blue-500 text-white">
+                Monitoring  
+            </a>
+        </div>
+        <a href="#" class="flex items-center justify-center w-full px-4 py-2 rounded-lg hover:bg-gray-100" style="height:50px;">
+            Input Anggaran
+        </a>
+    </nav>
+
+    <div class="mt-10 space-y-3 text-sm p-6">
+        <a href="#" class="block text-gray-600">Profil</a>
+        <a href="#" class="block text-red-500">Keluar</a>
+    </div>
+</aside>
