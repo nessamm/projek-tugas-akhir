0772b3380e18b15bd599d104b4f4c7f1a3a0e0a8
afwarifa <afwatikarifa@gmail.com>
2026-03-26 14:26:16 +0700

    feat: change typo controller

--- application/controllers/C_Master.php
+++ application/controllers/C_Master.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class C_MAster extends CI_Controller {
+class C_Master extends CI_Controller {
 
     public function __construct()
     {
